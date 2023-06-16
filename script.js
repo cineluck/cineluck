fa2bc039e63e791450ee71eab410668de2401fff
Cine Luck <136775023+cineluck@users.noreply.github.com>
2023-06-16 01:03:09 -0300

    Update 0.2 script.js

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ var filmes = [
       nome: 'Dá Tempo ao Tempo (2013)',
       urlPoster: 'https://images.justwatch.com/poster/190107318/s592/questao-de-tempo',
       sinopse: 'Aos 21 anos, Tim Lake descobre que consegue viajar no tempo… Após mais uma insatisfatória festa de Ano Novo, o pai de Tim revela-lhe que os homens da sua família sempre tiveram a capacidade de viajar no tempo. Tim não pode mudar a história, mas consegue alterar o que acontece ou aconteceu na sua própria vida - então, decide tornar o seu mundo um lugar melhor… arranjando uma namorada. Infelizmente, esse feito tornou-se mais complicado do que previa. Ao mudar-se de Cornwall para Londres, para estudar advocacia, Tim finalmente conhece a belíssima, mas insegura, Mary. Apaixonam-se, mas um lamentável incidente nas viagens pelo tempo faz com que ele nunca a tenha conhecido. Assim, voltam a encontrar-se pela primeira vez, várias vezes, até que, finalmente, depois de muita astúcia a viajar no tempo, ele conquista o seu coração.'
-    }
+    },
+    {
+      nome: 'Tokyo Revengers (2021)',
+      urlPoster: 'https://images.justwatch.com/backdrop/302216269/s640',
+      sinopse: 'Takemichi Hanagaki é um freelancer que atingiu o abismo absoluto de desespero em sua vida. Ele descobre que a única namorada que já teve, no ensino médio, Hinata Tachibana, foi morta pela implacável gangue Tokyo Manji. Um dia depois de saber de sua morte,
+ele está parado na plataforma da estação e acaba sendo empurrado para os trilhos por uma manada de pessoas. Ele fecha os olhos pensando que está prestes a morrer, mas quando abre os olhos de volta, ele de alguma forma voltou no tempo 12 anos. Agora que voltou a viver os melhores dias da sua vida, Takemichi decide vingar-se da sua vida.'
+    },
     
   ];
   
