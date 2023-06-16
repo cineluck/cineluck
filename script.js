83fd4a2d0e075891eb25536eb7f7904a4fbae297
Cine Luck <136775023+cineluck@users.noreply.github.com>
2023-06-16 19:56:05 -0300

    Versao 5

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const apiKey = "b0677543f985d49b82736273d298e2bd";
 const baseUrl = "https://api.themoviedb.org/3";
 
-const gerarFilmeAleatorio = () => {
+const gerarFilmeOuSerieAleatoria = () => {
   const totalPages = 1000; // Defina um número grande para cobrir todo o banco de dados
   const randomPage = Math.floor(Math.random() * totalPages) + 1;
 
-  fetch(`${baseUrl}/discover/movie?api_key=${apiKey}&language=pt-BR&page=${randomPage}`)
+  const mediaTypes = ["movie", "tv"];
+  const randomMediaType = mediaTypes[Math.floor(Math.random() * mediaTypes.length)];
+
+  const apiEndpoint = `${baseUrl}/discover/${randomMediaType}?api_key=${apiKey}&language=pt-BR&page=${randomPage}`;
+
+  fetch(apiEndpoint)
     .then(response => response.json())
     .then(data => {
-      const filmes = data.results;
-      const filmeAleatorio = filmes[Math.floor(Math.random() * filmes.length)];
+      const results = data.results;
+      const mediaAleatoria = results[Math.floor(Math.random() * results.length)];
       const posterElement = document.querySelector(".poster-container img");
       const nomeElement = document.querySelector("#filme-nome");
       const sinopseElement = document.querySelector("#filme-sinopse");
 
-      posterElement.src = `https://image.tmdb.org/t/p/w500${filmeAleatorio.poster_path}`;
-      nomeElement.textContent = filmeAleatorio.title;
-      sinopseElement.textContent = filmeAleatorio.overview;
+      posterElement.src = `https://image.tmdb.org/t/p/w500${mediaAleatoria.poster_path}`;
+      nomeElement.textContent = mediaAleatoria.title || mediaAleatoria.name;
+      sinopseElement.textContent = mediaAleatoria.overview;
+
+      // Habilitar ou desabilitar o botão "Falta Sinopse" com base na presença de sinopse
+      const faltaSinopseButton = document.getElementById("pesquisar-sinopse");
+      faltaSinopseButton.disabled = !mediaAleatoria.overview;
     })
     .catch(error => {
       console.log(error);
     });
 };
 
-document.getElementById("gerar").addEventListener("click", gerarFilmeAleatorio);
+document.getElementById("gerar").addEventListener("click", gerarFilmeOuSerieAleatoria);
 
 document.getElementById("tweetar").addEventListener("click", () => {
   const nomeFilme = document.querySelector("#filme-nome").textContent;
@@ -31,4 +40,12 @@ document.getElementById("tweetar").addEventListener("click", () => {
   window.open(urlTwitter);
 });
 
-gerarFilmeAleatorio();
+document.getElementById("pesquisar-sinopse").addEventListener("click", () => {
+  const nomeFilme = document.querySelector("#filme-nome").textContent;
+  const urlPesquisa = `https://www.google.com/search?q=${encodeURIComponent(
+    nomeFilme + " sinopse"
+  )}`;
+  window.open(urlPesquisa, "_blank");
+});
+
+gerarFilmeOuSerieAleatoria();
