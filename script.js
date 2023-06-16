98385c22caa2344cae3734eac072bbb296f8ff93
Cine Luck <136775023+cineluck@users.noreply.github.com>
2023-06-16 18:58:23 -0300

    Versao 3

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const apiKey = "b0677543f985d49b82736273d298e2bd";
 const baseUrl = "https://api.themoviedb.org/3";
 
 const gerarFilmeAleatorio = () => {
-  const totalPages = 20; // Defina o número total de páginas que você deseja buscar
+  const totalPages = 1000; // Defina um número grande para cobrir todo o banco de dados
   const randomPage = Math.floor(Math.random() * totalPages) + 1;
 
-  fetch(`${baseUrl}/movie/popular?api_key=${apiKey}&language=pt-BR&page=${randomPage}`)
+  fetch(`${baseUrl}/discover/movie?api_key=${apiKey}&language=pt-BR&page=${randomPage}`)
     .then(response => response.json())
     .then(data => {
       const filmes = data.results;
       const filmeAleatorio = filmes[Math.floor(Math.random() * filmes.length)];
-      const posterElement = document.querySelector("#poster img");
+      const posterElement = document.querySelector(".poster-container img");
       const nomeElement = document.querySelector("#filme-nome");
       const sinopseElement = document.querySelector("#filme-sinopse");
 
@@ -27,7 +27,7 @@ document.getElementById("gerar").addEventListener("click", gerarFilmeAleatorio);
 
 document.getElementById("tweetar").addEventListener("click", () => {
   const nomeFilme = document.querySelector("#filme-nome").textContent;
-  const urlTwitter = `https://twitter.com/compose/tweet?text=Acabei%20de%20assistir%20"${nomeFilme}"%20no%20Cine%20Luck`;
+  const urlTwitter = `https://twitter.com/compose/tweet?text=Acabei%20de%20aceitar%20uma%20indicação%20do%20@cineluck%20para%20assistir%20"${nomeFilme}"`;
   window.open(urlTwitter);
 });
 
